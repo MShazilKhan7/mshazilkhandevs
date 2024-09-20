6b5ebf408aa39a2298d8e3f097f83e10e4c67e67
MShazilKhan7 <shazilk14@gmail.com>
2024-09-20 19:34:26 +0500

    Category Section Added..

--- src/components/ArticleSection/ArticleSection.tsx
+++ src/components/ArticleSection/ArticleSection.tsx
@@ -1,18 +1,20 @@
 "use Client";
 import React from "react";
 import DisplayCard from "../DisplayCard/DisplayCard";
+import CategoriesBarSection from "../CategoriesBarSection/CategoriesBarSection";
 const ArticleSection = () => {
   return (
     <div className="max-[800px]:w-full w-[740px] px-4 text-white article-section pt-32  flex flex-col gap-4">
       <div className="header text-white px-2">
-        <h2 className="text-5xl font-bold text-white ">Articles</h2>
+        <h2 className="text-5xl font-bold text-white ">Articles.</h2>
         <div className="py-4">
           <p>
-            Below are my well researched articles written to help you to learn
-            concepts from multiple perspectives
+            Explore my carefully crafted articles, offering you a
+            multi-dimensional approach to mastering concepts.
           </p>
         </div>
       </div>
+      <CategoriesBarSection />
       <div className="flex flex-col gap-2">
         <DisplayCard isArticleCard />
         <DisplayCard isArticleCard />
