c658a5951bdfdd2d260260c9050df40f40ba0ae5
MShazilKhan7 <shazilk14@gmail.com>
2025-01-03 03:30:48 +0500

    Refactor article components for improved structure and functionality; rename page component, implement async data fetching, and enhance PortableText rendering.

--- src/app/(content)/content/page.tsx
+++ src/app/(content)/content/page.tsx
@@ -1,46 +1,18 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import VisualContentDialog from "@/components/VisualContentDialog/VisualContentDialog";
-import ContentCard from "@/components/ContentCard/ContentCard";
-import { contentPosts } from "@/constants/VisualContent/visualContentConfig";
-const VisualContent = () => {
-  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
-  const [slides, setSlides] = useState<string[]>([""]);
-  const onOpenChange = () => {
-    console.log("called.");
-    setDialogOpen(!dialogOpen);
-  };
+import React from "react";
+import ContentSection from "@/components/ContentSection/ContentSection";
+import { fetchDataFromSanity } from "@/lib/fetch";
+import { contentPosts } from "@/sanity/lib/queries";
+
+const VisualContent = async () => {
+  const posts = await fetchDataFromSanity({
+    query: contentPosts,
+  });
+
   return (
-    <div className="px-6 py-4">
-      <div className="py-2 pb-8 text-center">
-        <h2 className="tracking-tight text-white font-bold text-[35px]">
-          Welcome to My Content Space. Learn Interactively
-        </h2>
-      </div>
-      <div className="w-full flex flex-wrap gap-8 md:justify-center lg:justify-start ">
-        {contentPosts.map((content) => {
-          return (
-            <ContentCard
-              ContentCardDetails={content}
-              onOpenChange={() => {
-                setSlides(content.contentSlides);
-                onOpenChange();
-              }}
-            />
-          );
-        })}
-        <VisualContentDialog
-          open={dialogOpen}
-          onOpenChange={onOpenChange}
-          slides={slides}
-        />
-      </div>
+    <div>
+      <ContentSection posts={posts} />
     </div>
   );
 };
-
-{
-  /* <button onClick={onOpenChange}>open</button> */
-}
 
 export default VisualContent;
---
+++ src/components/ArticleSection/Article/ArticleFooter/ArticleFooter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ArticleFooter() {
+  return <div>{/* here we are going to write article footer dets.  */}</div>;
+}
+
+export default ArticleFooter;
