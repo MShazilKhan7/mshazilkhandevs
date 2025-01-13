c474bd4da2107acb4955215c9b5dc516b474ac6b
MShazilKhan7 <shazilk14@gmail.com>
2025-01-13 19:42:23 +0500

    Enhance styling for Home component with light and dark text support; refactor StudioPage to streamline imports and maintain static rendering

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -6,14 +6,11 @@
  * You can learn more about the next-sanity package here:
  * https://github.com/sanity-io/next-sanity
  */
+import { NextStudio } from "next-sanity/studio";
+import config from "../../../../sanity.config";
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
-
-export const dynamic = 'force-static'
-
-export { metadata, viewport } from 'next-sanity/studio'
+export const dynamic = "force-static";
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <NextStudio config={config} />;
 }
